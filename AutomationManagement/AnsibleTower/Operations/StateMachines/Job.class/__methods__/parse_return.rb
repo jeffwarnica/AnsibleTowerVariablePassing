912e5653aea7049c9f3605537439c59b27d895e2
Jeff Warnica <jwarnica@redhat.com>
2018-05-21 14:09:11 -0300

    Test with set_stats

--- AutomationManagement/AnsibleTower/Operations/StateMachines/Job.class/__methods__/parse_return.rb
+++ AutomationManagement/AnsibleTower/Operations/StateMachines/Job.class/__methods__/parse_return.rb
@@ -32,14 +32,14 @@ module AnsibleTowerVariablePassing
                     log(:info, "End of job dump")
                   end
 
-                  cfme_stdout = job.raw_stdout
+                  job_results = get_tower_job_results(job.ems_ref)
+                  if @debug
+                    log(:info, "start of job results dump")
+                    dump_thing(job_results)
+                    log(:info, "End of job results dump")
+                  end
 
-                  tower = @job.ext_management_system
-                  tower_user = tower.authentication_userid
-                  tower_pass = tower.authentication_password
-                  tower_name = tower.name
-                  log(:info, "Got a tower: [#{tower_name}] with username: [#{tower_user}] annd pass of size: [#{tower_pass.length}]") if @debug
-                  
+                  cfme_stdout = job.raw_stdout
 
                   if cfme_stdout[0, REST_TRIGGER.length] == REST_TRIGGER
                     log(:info, 'Tower hates us, we need to REST the stdout')
@@ -95,6 +95,51 @@ module AnsibleTowerVariablePassing
                   job
                 end
 
+                # Get Job attributes from Ansible Tower
+                # Function:
+                #      Performs a direct REST call against Ansible Tower to get job results
+                # Returns:
+                #      A the json parsed job result
+                # Requires:
+                #      tower_job_id - this is the id of the job in ansible tower - NOT the internal MIQ job ID
+                #           hint: job.ems_ref
+                #
+                def get_tower_job_results(tower_job_id)
+                  begin
+                    function_name = 'get_tower_job_results'.freeze
+
+                    # Pull tower details from the tower object
+                    tower = @job.ext_management_system
+                    tower_user = tower.authentication_userid
+                    tower_pass = tower.authentication_password
+                    tower_url  = tower.url
+                    tower_name = tower.name
+                    log(:info, "Got a tower: [#{tower_name}] with username: [#{tower_user}]") if @debug
+                    log(:info, "\tand pass of size: [#{tower_pass.length}] at [#{tower_url}]") if @debug
+
+
+                    # Verify username and password are set
+                    if tower_pass && tower_pass
+
+                      # set the parameters for the API call
+                      params = {
+                          :method => :get,
+                          :url => "#{tower_url}/jobs/#{tower_job_id}",
+                          :verify_ssl => false,
+                          :headers => { :authorization => "Basic #{Base64.strict_encode64("#{tower_user}:#{tower_pass}")}" }
+                      }
+
+                      # Query Tower via REST
+                      response = RestClient::Request.new(params).execute
+                    else
+                      raise "Function: #{function_name} unable to get credentials from tower object."
+                    end
+                  rescue => err
+                    log(:error, "Function: #{function_name}, Error: #{err}. Returning nil value")
+                    return nil
+                  end
+                  response
+                end
 
                 # Get STDOUT from Job Directly from Ansible Tower
                 # Function:
@@ -111,25 +156,22 @@ module AnsibleTowerVariablePassing
                     function_name = "get_stdout_from_tower_via_rest"
 
                     # Pull tower details from the tower object
-                    # tower = @job.manager
+                    tower = @job.ext_management_system
+                    tower_user = tower.authentication_userid
+                    tower_pass = tower.authentication_password
+                    tower_url  = tower.url
+                    tower_name = tower.name
+                    log(:info, "Got a tower: [#{tower_name}] with username: [#{tower_user}]") if @debug
+                    log(:info, "\tand pass of size: [#{tower_pass.length}] at [#{tower_url}]") if @debug
 
-                    tower_user = "admin" #@job.authentication_userid
-                    tower_pass = "redhat" #@job.authentication_password
-                    tower_name = "hard coded name" #@job.name
-
-                    # Log the tower name
-                    log(:info, "Executing Rest Call Using Tower: #{tower_name}")
 
                     # Verify username and password are set
                     if tower_pass && tower_pass
 
-                      # Pull API URL from variables domainn
-                      tower_api_url = "https://10.17.193.188/api/v2" #tower.url
-
                       # set the parameters for the API call
                       params = {
                           :method => :get,
-                          :url => "#{tower_api_url}/jobs/#{tower_job_id}/stdout/?format=txt_download",
+                          :url => "#{tower_url}/jobs/#{tower_job_id}/stdout/?format=txt_download",
                           :verify_ssl => false,
                           :headers => { :authorization => "Basic #{Base64.strict_encode64("#{tower_user}:#{tower_pass}")}" }
                       }
